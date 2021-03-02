dc80356ef71000326deb6f1f643d2dd9729ad206
Clown Monster <mk6386223@gmail.com>
2021-03-02 08:39:14 +0530

    search bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import "./styles.css";
+import styled from "styled-components";
 import SearchBar from "./components/searchbar";
 import TitleBar from "./components/Title";
+import Images from "./components/images";
 
 export default function App() {
+  const Grid = styled.div`
+    display: grid;
+    padding: 2rem;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 2rem;
+  `;
+
   return (
     <div className="App">
       <div>
         <TitleBar />
         <SearchBar />
+        <Grid>
+          {[...Array(50).keys()].map((i) => (
+            <Images
+              key={i}
+              src={`https://picsum.photos/1000/1000?random=${i}`}
+              alt={`Random image ${i}`}
+            />
+          ))}
+        </Grid>
       </div>
     </div>
   );
--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,14 +1,38 @@
 import React from "react";
 import "./components.css";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
 
 const SearchBar = () => {
   return (
     <div className="S-container">
       <div className="S-box">
-        <input type="text" placeholder="Search " />
+        <Autocomplete
+          className="box"
+          placeholder="Search"
+          options={Imgs.map((option) => option.title)}
+          renderInput={(params) => (
+            <TextField
+              className="inputTosearch"
+              {...params}
+              label="Search"
+              InputProps={{ ...params.InputProps, type: "search" }}
+            />
+          )}
+        />
       </div>
     </div>
   );
 };
+
+const Imgs = [
+  { title: "flower" },
+  { title: "Volopay" },
+  { title: "Bangalore" },
+  { title: "Rose" },
+  { title: "Weather" },
+  { title: "Cloud" },
+  { title: "sky" }
+];
 
 export default SearchBar;
